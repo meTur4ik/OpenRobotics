ef6ec9c80ac7998843627c29bdd3154dc1d85483
You <You@example.com>
2020-07-12 16:25:24 +0300

    make it look like lua style guide

--- autocraft/autocraft.lua
+++ autocraft/autocraft.lua
@@ -3,19 +3,19 @@ local meController = component.me_controller
 local shell = require('shell')
 
 function wait(operation)
-    while not operation.isDone()
-    do
+    while not operation.isDone() do
+        if operation.isCancelled() then
+            print('not enough resources')
+        end
     end
     print('done')
 end
 
 local args, options = shell.parse(...)
 
-if not args[1]
-then
+if not args[1] then
     local cr = meController.getCraftables()
-    for i=1, cr.n
-    do
+    for i=1, cr.n do
         print(i, cr[i].getItemStack().label)
     end
     os.exit()
@@ -24,16 +24,13 @@ end
 local cr = meController.getCraftables()
 local craftOp = nil
 
-if args[1] and args[2]
-then
+if args[1] and args[2] then
     local num = tonumber(args[1])
     local amount = tonumber(args[2])
     craftOp = cr[num].request(amount)
-else
-    if args[1]
-    then
-        craftOp = cr[num].request(1)
-    end
+elseif args[1] then
+    local num = tonumber(args[1])
+    craftOp = cr[num].request(1)
 end
 
 wait(craftOp)
