c1ef92f6a5a92e04c8c029f2ce6eb4745bca01d4
You <You@example.com>
2020-07-16 19:03:41 +0300

    begin iniconfig

---
+++ iniconfig/iniconfig.lua
@@ -0,0 +1,19 @@
+local io = require('io')
+
+local function initConfig()
+    local function parseString(str, obj)
+        print('dorova')
+    end
+
+    return {
+        readFile = function()
+            
+        end
+
+        readStringArray = function()
+            parseString({}, {})
+        end
+    }
+end
+
+return iniConfig()
