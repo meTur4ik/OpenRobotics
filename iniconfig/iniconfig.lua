e366ab3bf6e174b569000abc8b8b33c8c96408ec
You <You@example.com>
2020-07-24 21:45:15 +0300

    add iniconfig read and write

--- iniconfig/iniconfig.lua
+++ iniconfig/iniconfig.lua
@@ -1,19 +1,70 @@
-local io = require('io')
+function readStringArray(arr)
+    local o = {}
+    local lastSection = nil
 
-local function initConfig()
-    local function parseString(str, obj)
-        print('dorova')
+    function parseStr(str)
+        local key, value = str:match '^(%w+)%s*=%s*([%w./#*\\-]+)$' -- key=value
+        local section = str:match '^%[%s*(%w+)%s*%]$' -- [section]
+
+        if section then
+            o[section] = {}
+            lastSection = section
+        elseif key and value then
+            o[lastSection][key] = value
+        end
     end
 
-    return {
-        readFile = function()
-            
-        end,
+    for count = 1, #arr do
+        parseStr(arr[count])
+    end
 
-        readStringArray = function()
-            parseString({}, {})
-        end
-    }
+    return o
 end
 
-return iniConfig()
+function writeStringArray(obj)
+    local strs = {}
+    
+    for key, value in pairs(obj) do
+        table.insert(strs, '[' .. tostring(key) .. ']')
+        for key, value in pairs(value) do
+            table.insert(strs, tostring(key) .. '=' .. tostring(value))
+        end
+    end
+
+    return strs
+end
+
+function toFile(obj, filename)
+    local strs = writeStringArray(obj)
+    local file = io.open(filename, "w")
+    for i = 1, #strs do
+        file:write(strs[i]..'\n')
+        --print(strs[i])
+    end
+    file:flush()
+    file:close()
+end
+
+function fromFile(filename)
+    local file = io.open(filename)
+    local strs = {}
+
+    repeat
+        local str = file:read()
+        if str then
+            table.insert(strs, str)
+        end
+    until not str
+
+    file:close()
+    local obj = readStringArray(strs)
+
+    return obj
+end
+
+return {
+    ['deserialize'] = readStringArray,
+    ['serialize'] = writeStringArray,
+    ['toFile'] = toFile,
+    ['fromFile'] = fromFile
+}
